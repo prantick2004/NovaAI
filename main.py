acdb45366da5dff499fe360236040d5ca7addb2a
prantick <prantick2004@gmail.com>
2025-06-26 22:29:03 +0530

    add open youtube features

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import speech_recognition as sr
 import os
+import webbrowser
 
 def say(text):
     os.system(f'espeak "{text}"')
@@ -7,14 +8,21 @@ def say(text):
 def takecammand():
     r = sr.Recognizer() # Recognizer() is a class from the speech_recognition library. It creates an object (r) that: 1.Can listen to audio 2.Can understand and convert speech into text
     with sr.Microphone() as source: #opens your default microphone for listening.
-        r.pause_threshold = 2 #: If the user stops speaking for more than 1 second, it assumes you're done.
+        r.pause_threshold = 1 #: If the user stops speaking for more than 1 second, it assumes you're done.
         audio = r.listen(source) #This records the audio using the microphone.
-        query = r.recognize_google(audio, language='en-in') #This sends the audio to Google's Speech Recognition API to converts the spoken voice into text.
-        print(f'user said: {query}')
-        return query
+        try :
+            query = r.recognize_google(audio, language='en-in') #This sends the audio to Google's Speech Recognition API to converts the spoken voice into text.
+            print(f'user said: {query}')
+            return query
+        except Exception as e:
+            return " sorry some erroe occurred"
 if __name__ == '__main__':
     print('PyCharm')
     say("hii am Nova A.I. How can I help you")
-    print("listnning....")
-    text = takecammand()
-    say(text)
+    while True:
+     print("listnning....")
+     query = takecammand()
+     if "open youtube".lower() in query.lower():
+         say("open youtube")
+         webbrowser.open("https://youtube.com")
+     #say(text)

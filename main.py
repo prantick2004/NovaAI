65320d166cc76493768d04577fe89845087c97da
prantick <prantick2004@gmail.com>
2025-06-26 21:30:38 +0530

    change the listnning time

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def say(text):
 def takecammand():
     r = sr.Recognizer() # Recognizer() is a class from the speech_recognition library. It creates an object (r) that: 1.Can listen to audio 2.Can understand and convert speech into text
     with sr.Microphone() as source: #opens your default microphone for listening.
-        r.pause_threshold = 1 #: If the user stops speaking for more than 1 second, it assumes you're done.
+        r.pause_threshold = 2 #: If the user stops speaking for more than 1 second, it assumes you're done.
         audio = r.listen(source) #This records the audio using the microphone.
         query = r.recognize_google(audio, language='en-in') #This sends the audio to Google's Speech Recognition API to converts the spoken voice into text.
         print(f'user said: {query}')
